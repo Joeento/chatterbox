cf8d8ebd42d1887eb838332222aba911b3e8f0d3
Eric Kudler <joeento@gmail.com>
2014-09-28 15:42:55 -0400

    Moved controllers to single file for now

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,5 +1,16 @@
 'use strict';
 
+angular.module('myApp.view1', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+}]).controller('View1Ctrl', [function() {
+
+}]);
+
 angular.module('myApp.view2', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
